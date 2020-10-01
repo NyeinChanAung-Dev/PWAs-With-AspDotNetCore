7d2e5069129a24e4746e98445df6fcbc2c87d37b
Nyein Chan Aung <ncacu.monywa@gmail.com>
2020-10-01 20:11:16 +0630

    Add project files.

---
+++ PWAs-With-AspDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PWAs_With_AspDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
